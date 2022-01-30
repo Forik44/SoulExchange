796a9be9d357323e702ae6b1c0a8d531e010b1f3
Egor Prokofev <egor.prokofev.ru@gmail.com>
2022-01-30 17:07:26 +0300

    underlined edges

---
+++ Source/SoulExchange/InteractiveItems.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractiveItems.h"
+
+// Sets default values
+AInteractiveItems::AInteractiveItems()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInteractiveItems::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInteractiveItems::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
